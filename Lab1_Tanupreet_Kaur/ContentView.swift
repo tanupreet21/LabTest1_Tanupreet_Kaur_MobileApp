99aedff7a2b791511cb7f606c59fa4c6b2b56116
tanupreetkaur <tanubal21@gmail.com>
2026-02-17 11:21:43 -0500

    Initial Commit

---
+++ Lab1_Tanupreet_Kaur/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab1_Tanupreet_Kaur
+//
+//  Created by Tanupreet Kaur on 2026-02-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_Tanupreet_Kaur/Lab1_Tanupreet_KaurApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Tanupreet_KaurApp.swift
+//  Lab1_Tanupreet_Kaur
+//
+//  Created by Tanupreet Kaur on 2026-02-17.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Tanupreet_KaurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
